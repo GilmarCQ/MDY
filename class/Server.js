623371327607ee586679143ee420a5a42692ef40
unknown <gacampanaq@gmail.com>
2020-10-26 15:03:38 -0500

    'Cors'

--- class/Server.js
+++ class/Server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // const { employeRouter } = require('../routes/Employe');
 const { reniecRouter } = require('../routes/Reniec');
