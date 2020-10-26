7e67105c77285382de9aa8ce9ffcf333cca20fe7
unknown <gacampanaq@gmail.com>
2020-10-26 12:08:50 -0500

    'Cors'

--- class/Server.js
+++ class/Server.js
@@ -18,11 +18,10 @@ class Server {
 
     habilitarCors() {
         this.app.use((req, res, next) => {
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Headers',
-                'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-            res.setHeader('Allow', 'GET, POST, OPTION, PUT, DELETE');
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+            res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
             next();
         });
     }
