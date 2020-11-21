c51ed5455fac65a0f52e0b72f59e5c65db92d77f
unknown <gacampanaq@gmail.com>
2020-11-21 16:41:21 -0500

    Actualizacion

--- config/Sequelize.js
+++ config/Sequelize.js
@@ -7,20 +7,20 @@ const sedeModel = require('../models/Sede');
 const personaBeneficiarioModel = require('../models/PersonaBeneficiario');
 const asociacionModel = require('../models/Asociacion');
 
-// const conexion = new Sequelize(
-//     'mdyDB', 'mdy', 'qazWSX123456', {
-//         host: '192.168.1.3',
-//         dialect: 'postgres',
-//         port: 5432
-//     }
-// );
 const conexion = new Sequelize(
-    'mdy_apps', 'postgres', 'root', {
-        host: 'localhost',
+    'mdyDB', 'mdy', 'qazWSX123456', {
+        host: '192.168.1.3',
         dialect: 'postgres',
         port: 5432
     }
 );
+// const conexion = new Sequelize(
+//     'mdy_apps', 'postgres', 'root', {
+//         host: 'localhost',
+//         dialect: 'postgres',
+//         port: 5432
+//     }
+// );
 
 // try {
 //     await conexion.authenticate();
