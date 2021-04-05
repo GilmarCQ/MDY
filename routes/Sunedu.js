86bbe3a0ba0dfc840e9380a523730f3695a12c22
unknown <gacampanaq@gmail.com>
2021-04-05 12:28:11 -0500

    Correccion Minedu API

--- routes/Sunedu.js
+++ routes/Sunedu.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Sunedu = require('../controllers/Sunedu');
 const suneduRouter = express.Router();
 
-mineduRouter.get('/grados', Sunedu.getGradosTitulos);
+suneduRouter.get('/grados', Sunedu.getGradosTitulos);
 
 module.exports = {
     suneduRouter
